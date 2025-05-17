ec4ce78881bf789623717733565441aef4649505
Cingiz <chingizaa-bb208@code.edu.az>
2025-05-17 16:08:53 +0400

    added

---
+++ ClinicMvcPr/Program.cs
@@ -0,0 +1,23 @@
+namespace ClinicMvcPr
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+
+
+            var app = builder.Build();
+            app.UseStaticFiles();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=index}/{id?}"
+                );
+            
+
+            app.Run();
+        }
+    }
+}
